ba6d8c14d9c72b773dd75186a7874396b22f43ab
usuario talleres SO - dc.uba.ar <tallerso@taller-so>
2025-04-07 21:54:01 -0300

    some more work on task 2

--- Ejercicio2/anillo_alu.c
+++ Ejercicio2/anillo_alu.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <time.h>
@@ -10,15 +11,25 @@ int generate_random_number(){
 	return (rand() % 50);
 }
 
-int hijo(int id, int n, int numero_inicial, int proceso_inicial, int** pipe_ids){
+int hijo(int id, int n, int numero_inicial, int proceso_inicial, int** pipe_ids, bool es_primera_llamda){
+	printf("soy hijo %d", id);
 	int received_num;
-	if (proceso_inicial == id) {
+	int numero_secreto;
+	if (proceso_inicial == id && es_primera_llamda) {
 		received_num = numero_inicial;
+		numero_secreto = generate_random_number();
 	} else {
 		received_num = read(pipe_ids[(id - 1 + n) % n], received_num, sizeof(received_num));
+		printf("Proceso %d received numero %d", id, received_num);
 	}
-
-	write(pipe_ids[id % n][PIPE_WRITE], (received_num + 1), sizeof(numero_inicial));
+	// proceso inical esta llamado otra vez, pero no es la primera llamada
+	if (proceso_inicial == id && !es_primera_llamda) {
+		// exit(1);
+	}
+	write(pipe_ids[(id + n) % n][PIPE_WRITE], (received_num + 1), sizeof(numero_inicial));
+	// reiniciar y esperar el proximo numero
+	// hijo(id, n, numero_inicial, proceso_inicial, pipe_ids, false);
+	exit(1);
 }
 
 int main(int argc, char **argv)
@@ -26,7 +37,7 @@ int main(int argc, char **argv)
 	//Funcion para cargar nueva semilla para el numero aleatorio
 	srand(time(NULL));
 
-	int status, pid, n, start, buffer;
+	int status, n, start, buffer;
 	n = atoi(argv[1]);
 	buffer = atoi(argv[2]);
 	start = atoi(argv[3]);
@@ -43,12 +54,8 @@ int main(int argc, char **argv)
 	}
 
 	// crear a n hijos
-	pid_t* pids[n];
-	pid_t pid;
-	int process_id;
-	for (process_id = 0; process_id < n; process_id ++){
-		pid = fork();
-		if (pid == 0) hijo(process_id, n, buffer, start, pipe_ids);
-		else pids[process_id] = pid;
+	for (int process_id = 0; process_id < n; process_id ++){
+		int pid = fork();
+		if (pid == 0) hijo(process_id, n, buffer, start, pipe_ids, true);
 	}
 }
